4ec635b598a4d0d06c997d74dc851b2d8506b7a4
Zamboomafoo <robsmith812@gmail.com>
2019-04-19 15:58:07 -0700

    tables updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,43 @@
 INSERT INTO strains (strain_name, image_url) VALUES 
 ("3 Bears OG", "/assets/images/3bears.jpg"),
-("zilla", "/assets/images/nug.jpg"),
-("Zeta", "/assets/images/nug.jpg"),
-("Zero", "/assets/images/nug.jpg"),
-("AK-47", "/assets/images/nug.jpg");
+("541 Kush", "/assets/images/541kush.jpg"),
+("AK-47", "/assets/images/ak47.jpg"),
+("Acid Dough", "/assets/images/aciddough.jpg"),
+("Banana Hammock R1", "/assets/images/bhr1.jpg"),
+("Big Sur Holy Bud", "/assets/images/bigsur.jpg"),
+("Cadillac Purple", "/assets/images/cadpurp.jpg"),
+("Dirt Nap", "/assets/images/dirtnap.jpg"),
+("Extreme Cream", "/assets/images/cream.jpg"),
+("Green Crack Extreme", "/assets/images/greencrack.jpg"),
+("Hippie Chicken", "/assets/images/hippiechicken.jpg"),
+("Jack Skellington", "/assets/images/skellington.jpg"),
+("Killer Queen", "/assets/images/queen.jpg"),
+("Liquid Butter", "/assets/images/butter.jpg"),
+("Master Kush", "/assets/images/masterkush.jpg"),
+("Night Terror OG", "/assets/images/terror.jpg"),
+("Peyote Cookies", "/assets/images/peyotecookies.jpg"),
+("Sugar Breath", "/assets/images/sugar.jpg"),
+("Walter White", "/assets/images/ww.jpg"),
+("Zelly's Gift", "/assets/images/zgift.jpg");
 
 INSERT INTO strain_info (strain_fact, strain_type, strain_taste, strain_image, strain_id) VALUES
 ("3 Bears OG is a cross of Bear OG, Karma’s OG Cut, and Triangle Kush. This strain generates pungent, flavorful buds in a smaller, more manageable plant size.", "Indica", "Sweet, Pungent, Earthy", "/assets/images/3bears.jpg", 1),
-("Green", "Indica", "Watermelon", "/assets/images/nug.jpg", 2),
-("Sleepy", "Sativa", "Cherry", "/assets/images/nug.jpg", 3),
-("Bomb", "Sativa", "Vanilla", "/assets/images/nug.jpg", 4),
-("Fire", "Hybrid", "Chocolate", "/assets/images/nug.jpg", 5);
-
--- ("Sticky", "Indica", "Strawberry", "/assets/images/nug.jpg", 1),
-
-
--- SELECT strain_names.strain_name, strains_info.strain_info AS info FROM strain_names LEFT JOIN strains_info ON strains_info.strain_id = strain_names.id;
+("541 Kush is a known phenotype of Triangle Kush. Its buds are colorful, retaining hints of dark green and purple, as earthy kush notes mingle with an underlying diesel aroma. 541 Kush will knock even the most experienced consumers right out.", "Hybrid", "Earthy", "/assets/images/541kush.jpg", 2),
+("AK-47 will leave you relaxed and mellow. This sativa-dominant hybrid delivers a steady and long-lasting cerebral buzz that keeps you mentally alert and engaged in creative or social activities.", "Hybrid", "Earthy, Pungent, Woody", "/assets/images/ak47.jpg", 3),
+("Acid Dough is a sativa-dominant strain with energetic effects. This strain offers sweet, candy-like aromas with bright pineapple flavors and a generous yield.", "Sativa", "Earthy, Tree Fruit, Woody", "/assets/images/aciddough.jpg", 4),
+("Banana Hammock is a large dense purple flowers with fruity tropical terpenes smelling of grape and honey make for a delicious smoke. Great for settling down into the couch or getting onto a bike for a lazy afternoon adventure.", "Indica", "Citrus, Sweet, Flowery", "/assets/images/bhr1.jpg", 5),
+("Big Sur Holy Bud is a sativa bred. This plant has an indica structure, with purple/pink hued buds that offer a blueberry, menthol aroma and effects true to its sativa heritage.", "Sativa", "Sweet, Citrus, Pine", "/assets/images/bigsur.jpg", 6),
+("Cadillac Purple is a heavy indica strain that is popular among users seeking true body relaxation and pain relief without the spacey cerebral effects common to more sativa-heavy hybrids.", "Indica", "Grape, Woody, Berry", "/assets/images/cadpurp.jpg", 7),
+("Dirt Nap takes on an aroma and tranquilizing high and a sweet flavor while increasing resin production and bud size. Dirt Nap is a great strain for consumer seeking a sedative high that helps manage pain and anxiety.", "Hybrid", "Sweet", "/assets/images/dirtnap.jpg", 8),
+("Extreme Cream develops deep purple colas dusted with trichomes and emits a strong, earthy aroma that is somehow creamy on the nose and buttery on the palate; offering mood elevation that abates anxiety and spurs conversation.", "Indica", "Butter, Vanilla, Flowery", "/assets/images/cream.jpg", 9),
+("Green Crack Extreme’s dense buds will be bright green and covered in sugary crystals that fit with its sweet candy smell. Contrary to its extreme name, this strain is helpful in reducing stress and getting inspired.", "Indica", "Sweet, Earthy, Pungent", "/assets/images/greencrack.jpg", 10),
+("Hippie Chicken is a strain with strongly euphoric effects that border on psychedelic. It takes after its sativa parent in its giggly, energetic effects which are accented only slightly by the tingly, relaxing effects.", "Sativa", "Sweet, Tree Fruit, Earthy", "/assets/images/hippiechicken.jpg", 11),
+("With a fruity citrus and diesel aroma, Jack Skellington promises an intensely psychoactive experience that brings creativity and introspection to a new level.", "Sativa", "Earthy, Diesel, Pungent", "/assets/images/skellington.jpg", 12),
+("Lavender and purple hues tinge leaves and buds underneath a dusty coat of snow-white resin. This strain gets its name from the sour grape aroma its buds carry, which is accented by spicy, hashy undertones.", "Hybrid", "Grape, Tropical, Berry", "/assets/images/queen.jpg", 13),
+("Liquid Butter will have you melting into the couch or your favorite tub of ice cream. Patients who suffer from pain and anxiety typically have success with this strain, but it’s best to use Liquid Butter when you don’t have a lot to get done.", "Indica", "Butter, Sweet, Honey", "/assets/images/butter.jpg", 14),
+("The taste of Master Kush is reminiscent of the famous hard-rubbed charas hash. This strain holds a superb balance of full-body relaxation without the mind-numbing effect that many indica strains produce.", "Indica", "Earthy, Pungent, Woody", "/assets/images/masterkush.jpg", 15),
+("This strain induces relaxing effects that sink deeper in the body over time. You may find your eyelids feeling heavy before long, but don’t let a name like Night Terror OG fool you: a good night’s sleep is on its way.", "Indica", "Earthy, Berry, Sweet", "/assets/images/terror.jpg", 16),
+("Peyote Cookies is a vigorous hybrid flower that is easy to grow and pleasurable to consume. The flowering plant displays a dazzling ruby red and purple complexion coupled with delicious flavors of guava, vanilla, coffee, and earth.", "Indica", "Earthy, Sweet, Lime", "/assets/images/peyotecookies.jpg", 17),
+("Sugar Breath has beautiful dense buds coated in trichomes. The aroma is a sweet, gassy blend that also includes notes of pine, incense, vanilla, and cinnamon. The potent high may provide a relaxing evening of laughter and snacks.", "Hybrid", "Sweet, Pine, Vanilla", "/assets/images/sugar.jpg", 18),
+("Walter White has an aroma and taste are citrusy and creamy on the exhale.", "Hybrid", "Earthy, Woody, Citrus", "/assets/images/ww.jpg", 19),
+("The genetics root this strain in uplifting sativa effects while its recessive indica-dominant genetics imbue the plant with flavors of lime leaves and grape jelly. For consumers seeking an energizing boost that improves mood and helps combat depression.", "Sativa", "Lemon, Spicy/Herbal, Citrus", "/assets/images/zgift.jpg", 20);
