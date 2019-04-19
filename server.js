30b7a0d79880ba208965d07234dd50b5212a7874
Zamboomafoo <robsmith812@gmail.com>
2019-04-18 21:26:55 -0700

    added mystrains link

--- server.js
+++ server.js
@@ -28,8 +28,16 @@ app.get('/signin', function (req, res) {
 
 app.post('/signin', function (req, res) {
     console.log(req.body)
-    req.session.user = req.body
-    res.redirect('/profile')
+    connection.query('SELECT id FROM users WHERE user_name=? AND user_password=?', [req.body.user_name, req.body.user_password], function(err,result){
+        if (err) {
+            res.redirect('/signin')
+            return
+        }
+        req.session.user = req.body
+        req.session.user.id = result[0].id
+        console.log(req.session)
+        res.redirect('/profile')
+    })
 });
 
 app.get('/signup', function (req, res) {
@@ -72,7 +80,7 @@ app.get('/strains', function (req, res) {
 })
 
 app.get('/strain_info/:name', function (req, res){
-    connection.query('SELECT strains.strain_name, strain_info.strain_fact, strain_info.strain_type, strain_info.strain_taste, strain_info.strain_image FROM strains LEFT JOIN strain_info ON strain_info.strain_id = strains.id WHERE strains.strain_name = ?', [req.params.name],function (error, results, fields){
+    connection.query('SELECT strains.id, strains.strain_name, strain_info.strain_fact, strain_info.strain_type, strain_info.strain_taste, strain_info.strain_image FROM strains LEFT JOIN strain_info ON strain_info.strain_id = strains.id WHERE strains.strain_name = ?', [req.params.name],function (error, results, fields){
         if (error){
             res.send(error)
             return
@@ -84,7 +92,24 @@ app.get('/strain_info/:name', function (req, res){
         // res.send(results)
         console.log(results)
     })
-})
+});
+
+app.get('/my_strains', function (req, res){
+    console.log(req.session.user)
+    res.render('pages/my_strains', req.session.user)
+});
+// this is to insert into my trees on button click
+app.post("/my_strains/:id", function(req, res){
+
+    console.log('inside post my_strains ')
+    connection.query('INSERT INTO trees SET ?', {user_id: req.session.user.id, my_tree: req.params.id}, function (error, results, fields) {
+        if (error) 
+            res.send(error)
+        else {
+            res.redirect('/my_strains')
+        }
+    })
+});
 
 app.get('/', function (req, res) {
     res.render('pages/signin')
